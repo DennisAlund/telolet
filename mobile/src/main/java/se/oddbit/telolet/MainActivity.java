111367789fd13e4fcb53ff124ce25ba486111922
Dennis Alund <dennis.alund@gmail.com>
2017-01-06 13:37:24 +0800

    Adding basic functionality for logging in

--- mobile/src/main/java/se/oddbit/telolet/MainActivity.java
+++ mobile/src/main/java/se/oddbit/telolet/MainActivity.java
@@ -1,20 +1,43 @@
 package se.oddbit.telolet;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ui.ResultCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.crash.FirebaseCrash;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 42;
+
+    private View mRootView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRootView = findViewById(R.id.activity_root_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,27 +49,92 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        startSignInProcess();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_logout:
+                final Context context = this;
+                AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull final Task<Void> task) {
+                        PreferenceManager.getDefaultSharedPreferences(context).edit().clear().apply();
+                        startSignInProcess();
+                    }
+                });
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode != RC_SIGN_IN) {
+            return;
+        }
+
+        switch (resultCode) {
+            case RESULT_OK:
+                FirebaseCrash.logcat(Log.DEBUG, LOG_TAG, "Finished Firebase UI Auth Process Successfully");
+                break;
+
+            case RESULT_CANCELED:
+                FirebaseCrash.logcat(Log.DEBUG, LOG_TAG, "Cancelled Firebase UI Auth Process");
+                startSignInProcess();
+                break;
+
+            case ResultCodes.RESULT_NO_NETWORK:
+                FirebaseCrash.logcat(Log.DEBUG, LOG_TAG, "Failed Firebase UI Auth Process: no network");
+                Snackbar.make(mRootView, R.string.no_internet_connection, Snackbar.LENGTH_INDEFINITE).show();
+                startSignInProcess();
+                break;
+        }
+    }
+
+    private void startSignInProcess() {
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            final List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                    new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build());
+
+            final Intent loginIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setProviders(providers)
+                    .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                    .setLogo(R.mipmap.ic_launcher)
+                    .setTheme(R.style.AppTheme)
+                    .build();
+
+            startActivityForResult(loginIntent, RC_SIGN_IN);
+        }
+    }
+
+    @Override
+    public void onAuthStateChanged(@NonNull final FirebaseAuth firebaseAuth) {
+        final FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser == null) {
+            FirebaseCrash.logcat(Log.DEBUG, LOG_TAG, "onAuthStateChanged: NOT LOGGED IN");
+            return;
+        }
+
+        FirebaseCrash.logcat(Log.DEBUG, LOG_TAG, String.format(Locale.getDefault(), "onAuthStateChanged {method: '%s', email: '%s', uid: '%s'}",
+                firebaseUser.getProviderId(), firebaseUser.getEmail(), firebaseUser.getUid()));
+
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, firebaseUser.getProviderId());
+        FirebaseAnalytics.getInstance(this).logEvent(FirebaseAnalytics.Event.LOGIN, bundle);
     }
 }
