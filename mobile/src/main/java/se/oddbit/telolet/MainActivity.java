68eceabf0e596a4925bf96e9828baf04098df556
Dennis Alund <dennis.alund@gmail.com>
2017-01-08 16:27:22 +0800

    Removing FAB

--- mobile/src/main/java/se/oddbit/telolet/MainActivity.java
+++ mobile/src/main/java/se/oddbit/telolet/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -67,15 +66,6 @@ public class MainActivity extends AppCompatActivity implements FirebaseAuth.Auth
         mRootView = findViewById(R.id.activity_root_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         mCurrentUserListener = new CurrentUserListener(this);
 
