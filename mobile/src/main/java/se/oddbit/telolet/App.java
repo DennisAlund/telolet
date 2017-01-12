78b329037d6eb07f90358e17f6618a5b23bfe6fc
Dennis Alund <dennis.alund@gmail.com>
2017-01-12 17:23:05 +0800

    Removing some weird name refactoring false matching

--- mobile/src/main/java/se/oddbit/telolet/App.java
+++ mobile/src/main/java/se/oddbit/telolet/App.java
@@ -22,7 +22,6 @@ public class App extends Application {
         super.onCreate();
         FirebaseCrash.logcat(Log.DEBUG, LOG_TAG, "onCreate");
 
-        // PATH
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         if (BuildConfig.DEBUG) {
             FirebaseDatabase.getInstance().setLogLevel(Logger.Level.DEBUG);
@@ -39,9 +38,9 @@ public class App extends Application {
             @Override
             public void onComplete(@NonNull final Task<Void> task) {
                 if (!task.isSuccessful()) {
-                    FirebaseCrash.logcat(Log.ERROR, LOG_TAG, "Could not fetch PATH remote config. Will go with defaults");
+                    FirebaseCrash.logcat(Log.ERROR, LOG_TAG, "Could not fetch remote config. Will go with defaults");
                 } else {
-                    FirebaseCrash.logcat(Log.INFO, LOG_TAG, "Successfully fetched PATH remote configuration");
+                    FirebaseCrash.logcat(Log.INFO, LOG_TAG, "Successfully fetched remote configuration");
                     remoteConfig.activateFetched();
                 }
             }
